6f06c59cbb8b9a147ede50c96eb2b0cdf0599b29
Colin Lubner <clubner19@gmail.com>
2026-02-21 12:35:13 -0500

    Finished robot class for now

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,50 +7,92 @@ package frc.robot;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj.XboxController;
 
 public class Robot extends TimedRobot {
-  private final XboxController m_controller = new XboxController(0);
-  private final Drivetrain m_swerve = new Drivetrain();
+  private Command autonomousCommand;
+  private RobotContainer robotContainer;
 
-  // Slew rate limiters to make joystick inputs more gentle; 1/3 sec from 0 to 1.
-  private final SlewRateLimiter m_xspeedLimiter = new SlewRateLimiter(3);
-  private final SlewRateLimiter m_yspeedLimiter = new SlewRateLimiter(3);
-  private final SlewRateLimiter m_rotLimiter = new SlewRateLimiter(3);
+  /*
+   * This function is run when the robot is first started up
+   * and should be used for any initialization code.
+   */
+  //Overriding initialization
+  @Override
+  public void robotInit() {
+    //Instantiate RobotContainer. This performs all button bindings
+    //and puts autonomous chooser onto the dashboard.
+    robotContainer = new RobotContainer();
+  }
+  
+  /*
+   * This function is called every 20 ms regardless of the mode. Use for items like diagnostics
+   * you want run during disabled, autonomous, teleoperated, and test.
+   * 
+   * This runs after the mode specific periodic functions, but before LiveWindow
+   * and SmartDashboard integrated updating.
+   */
+  //Overriding different methods in TimedRobot
+  @Override
+  public void robotPeriodic() {
+    /*
+     * Runs scheduler. Polls buttons, adds newly-scheduled
+     * commands, runs already-scheduled commands, removes 
+     * finished or interrupted commands, runs subsystem periodic
+     * methods. Must be called from robot's periodic block in order
+     * for anything in Command-based framework to work.
+     */
+    CommandScheduler.getInstance().run();
+  }
+  //This function is called once each time the robot enters Disabled mode.
+  @Override
+  public void disabledInit() {}
+
+  @Override 
+  public void disabledPeriodic() {}
+
+  //This runs the autonomous command selected by RobotContainer class
+  @Override
+  public void autonomousInit() {
+    autonomousCommand = robotContainer.getAutonomousCommand();
+    //Schedule the autonomous command
+    if (autonomousCommand != null) {
+      autonomousCommand.schedule();
+    }
+  }
+
+  //This is called periodically during autonomous
+  @Override
+  public void autonomousPeriodic() {}
 
   @Override
-  public void autonomousPeriodic() {
-    driveWithJoystick(false);
-    m_swerve.updateOdometry();
+  public void teleopInit() {
+    /*
+     * This makes the autonomous stop running when
+     * teleop starts. If it is desirable for autonomous
+     * to continue until interrupted by another
+     * command, remove this line or comment it out.
+     */
+    if (autonomousCommand != null) {
+      autonomousCommand.cancel();
+    }
+
   }
+  
+  //This function is called periodically during operator control.
+  @Override
+  public void teleopPeriodic() {}
 
   @Override
-  public void teleopPeriodic() {
-    driveWithJoystick(true);
+  public void testInit() {
+    //Cancels all running commands at start of test mode
+    CommandScheduler.getInstance().cancelAll();
   }
 
-  private void driveWithJoystick(boolean fieldRelative) {
-    // Get the x speed. We are inverting this because Xbox controllers return
-    // negative values when we push forward.
-    final var xSpeed =
-        -m_xspeedLimiter.calculate(MathUtil.applyDeadband(m_controller.getLeftY(), 0.02))
-            * Drivetrain.kMaxSpeed;
+  //Function called periodically during test mode.
+  @Override
+  public void testPeriodic() {}
 
-    // Get the y speed or sideways/strafe speed. We are inverting this because
-    // we want a positive value when we pull to the left. Xbox controllers
-    // return positive values when you pull to the right by default.
-    final var ySpeed =
-        -m_yspeedLimiter.calculate(MathUtil.applyDeadband(m_controller.getLeftX(), 0.02))
-            * Drivetrain.kMaxSpeed;
-
-    // Get the rate of angular rotation. We are inverting this because we want a
-    // positive value when we pull to the left (remember, CCW is positive in
-    // mathematics). Xbox controllers return positive values when you pull to
-    // the right by default.
-    final var rot =
-        -m_rotLimiter.calculate(MathUtil.applyDeadband(m_controller.getRightX(), 0.02))
-            * Drivetrain.kMaxAngularSpeed;
-
-    m_swerve.drive(xSpeed, ySpeed, rot, fieldRelative, getPeriod());
-  }
 }
